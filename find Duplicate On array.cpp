28e8f5db3777d707a5b3a81fe9d2262285fb42ad
mohand reda sakr <mohandsakr69@gmail.com>
2017-08-15 15:08:58 +0200

    Add files via upload
    this is   implementation of an algorithm that remove the duplicates of array  it's  time complexity is O(n);

---
+++ find Duplicate On array.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+//find duplicate on array ;
+// Name        : find.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include<map>
+using namespace std;
+
+int main() {
+
+	int t;
+	cin>>t;
+	while(t--){
+	int n;
+	cin>>n;
+	map<int,int>ma;
+		int flag=0;
+		map<int,int>::iterator it;
+	for(int i=0;i<n;i++)
+	{int x;
+	cin>>x;
+		++ma[x];
+	}
+	 for(it=ma.begin();it!=ma.end();it++){
+		 if((*it).second>1){
+			 cout<<(*it).first<<" ";
+			 ++flag;
+		 }
+	 }
+	 if(flag==0)cout<<"-1";
+	 cout<<"\n";
+	}
+	return 0;
+}
